f98770f6cd961076cb9c4c4a13cdd36586fcd4e7
kriskiwinkaa@gmail.com <kriskiwinkaa@gmail.com>
2022-11-15 16:42:08 +0300

    add plagins

---
+++ games/content_generation/app.js
@@ -0,0 +1,32 @@
+const board = document.querySelector('#board');
+const colors = ['#5bcdb4', '#cdc35b', '#FF00FF', '#FF4500', '#808080', '#2F4F4F'];
+const SQUARES_NUMBER = 1344;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor(square));
+
+    square.addEventListener('mouseleave', () => removeColor(square));
+
+    board.append(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#615f5f89';
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
+
+//colors //transition //amount
